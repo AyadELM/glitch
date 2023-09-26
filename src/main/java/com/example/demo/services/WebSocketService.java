382da71292b5d33315338a48f2bc4abe049f83d5
Jouhaina <benarbia.jouhaina@gmail.com>
2023-09-26 16:39:34 +0100

    first commit

---
+++ src/main/java/com/example/demo/services/WebSocketService.java
@@ -0,0 +1,19 @@
+package com.example.demo.services;
+
+import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.stereotype.Service;
+
+@Service
+public class WebSocketService {
+
+    private final SimpMessagingTemplate messagingTemplate;
+
+    public WebSocketService(SimpMessagingTemplate messagingTemplate) {
+        this.messagingTemplate = messagingTemplate;
+    }
+
+    public void broadcastMessage(String message) {
+        messagingTemplate.convertAndSend("/topic/messages", message);
+    }
+}
+
